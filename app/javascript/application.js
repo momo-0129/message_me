23371002824cdca0c1187526a52eff1c197b9cbe
momo-0129 <tonyfang8733@gmail.com>
2023-03-07 15:44:02 +0900

    Install Semantic-UI for rails7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,34 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+import "semantic-ui"
+
+// = require semantic-ui
+
+/* 【修正版】Semantic UI用の jquery 関数を定義 */
+/* "turbo:load" */
+$(document).on("turbo:load", function() {
+
+  /* ここに必要な jquery 関数を追記 */
+
+  /* 例1）ドロップダウンを利用する場合 */
+  $('.ui.dropdown').dropdown();
+
+  /* 例2）アコーディオンを利用する場合 */
+  $('.ui.accordion').accordion();
+
+});
+
+/* "turbo:render" */
+$(document).on("turbo:render", function() {
+
+  /* ここに必要な jquery 関数を追記 */
+
+  /* 例1）ドロップダウンを利用する場合 */
+  $('.ui.dropdown').dropdown();
+
+  /* 例2）アコーディオンを利用する場合 */
+  $('.ui.accordion').accordion();
+
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
+pin "semantic-ui", to: "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"
