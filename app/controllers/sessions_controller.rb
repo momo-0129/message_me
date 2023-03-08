1d3c638160711cf64f9330b3fc6786fcca0b77dd
momo-0129 <tonyfang8733@gmail.com>
2023-03-08 16:35:23 +0900

    Add sessions create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else   
+      flash.new[:error] = "There was something wring with your login information"
+      render :new, status: :bad_request
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
 end
